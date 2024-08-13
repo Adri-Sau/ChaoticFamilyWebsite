fc1b2fa96db35ee993d208a89d383d598b20d9bc
Adriano Saurwein <nano2101@outlook.com>
2024-08-13 13:42:19 +0200

    Home page draft

---
+++ src/components/navigation/navbar.svelte
@@ -0,0 +1,51 @@
+<script>
+	import LOGO from '$lib/images/LogoChaoticTrasparente.png';
+</script>
+
+<div class="drawer drawer-end sticky top-0 z-50">
+	<input id="my-drawer-1" type="checkbox" class="drawer-toggle" />
+	<div class="drawer-content">
+		<div
+			class="p-2 px-4 full navbar bg-base-200 flex sticky top-0 z-20 shadow-[0_5px_2px_0_rgba(0,0,0,0.5)]"
+		>
+			<div class="navbar-start">
+				<a href="/">
+					<img src={LOGO} alt="Logo Chaotic Family" class="rounded w-16 hover:scale-110" />
+				</a>
+			</div>
+
+			<div class="navbar-center">
+				<ul class="menu menu-horizontal text-xl font-bold hidden sm:flex">Menu</ul>
+			</div>
+
+			<div class="navbar-end">
+				<label
+					for="my-drawer-1"
+					aria-label="open sidebar"
+					class="btn btn-square btn-ghost flex sm:hidden"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						class="inline-block w-6 h-6 stroke-current"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h16M4 18h16"
+						></path></svg
+					>
+				</label>
+			</div>
+		</div>
+
+		<!-- Page content here -->
+		<slot />
+	</div>
+
+	<div class="drawer-side">
+		<label for="my-drawer-1" aria-label="close sidebar" class="drawer-overlay"></label>
+		<ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content pt-20">menu \n search?</ul>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<script>
+	import Navbar from '../components/navigation/navbar.svelte';
+	import Footer from '../components/navigation/footer.svelte';
+
+	const content = [
+		{
+			image: 'https://picsum.photos/seed/random/200',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
+			align: 'left',
+			mask: 'mask-squircle'
+		},
+		{
+			image: 'https://picsum.photos/seed/ciao/200',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
+			align: 'right',
+			mask: 'mask-hexagon-2'
+		},
+		{
+			image: 'https://picsum.photos/seed/no/200',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
+			align: 'left',
+			mask: 'mask-circle'
+		},
+		{
+			image: 'https://picsum.photos/seed/prova/200',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
+			align: 'right',
+			mask: 'mask-square'
+		}
+	];
+</script>
+
+<Navbar />
+<div class="flex-grow">
+	<main class="container mx-auto my-8 grid grid-cols-1 gap-8">
+		{#each content as item}
+			<div
+				class="flex flex-col md:flex-row {item.align === 'left'
+					? 'md:flex-row-reverse'
+					: ''} items-center md:items-start"
+			>
+				<div class="md:w-1/4 p-4">
+					<img
+						src={item.image}
+						alt="Placeholder"
+						class={`w-full shadow-lg mask ${item.mask} ${item.mask === 'mask-square' ? 'rounded-lg' : ''}`}
+					/>
+				</div>
+				<div class="md:w-1/4 p-4">
+					<p class="text-lg font-semibold">{item.text}</p>
+				</div>
+			</div>
+		{/each}
+	</main>
+</div>
+<Footer />
