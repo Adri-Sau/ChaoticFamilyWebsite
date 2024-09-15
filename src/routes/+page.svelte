df1c36411ba33ae26214bc841ce51a89b8111559
Adriano Saurwein <nano2101@outlook.com>
2024-09-15 20:03:58 +0200

    homepage + json with data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,35 @@
 <script>
 	import Navbar from '../components/navigation/navbar.svelte';
 	import Footer from '../components/navigation/footer.svelte';
-	import Background from '../components/utils/background.svelte';
+	import data from '$lib/data.json';
 
-	const BLACK = 'https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg';
+	const BACKGROUND = data.home.background.src;
 
-	const content = [
-		{
-			image: 'https://picsum.photos/seed/random/200',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
-			align: 'left',
-			mask: 'mask-squircle'
-		},
-		{
-			image: 'https://picsum.photos/seed/ciao/200',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
-			align: 'right',
-			mask: 'mask-hexagon-2'
-		},
-		{
-			image: 'https://picsum.photos/seed/no/200',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
-			align: 'left',
-			mask: 'mask-circle'
-		},
-		{
-			image: 'https://picsum.photos/seed/prova/200',
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
-			align: 'right',
-			mask: 'mask-square'
-		}
-	];
+	const PARAGRAPHS = data.home.paragraphs;
 </script>
 
 <Navbar />
-<Background background="https://picsum.photos/seed/chaotic/1920/1080" />
 
-<div class="flex-grow">
+<div class="flex-grow bg-cover bg-center bg-fixed" style="background-image: url('{BACKGROUND}');">
 	<div class="container mx-auto my-8 grid grid-cols-1 gap-8">
-		{#each content as item}
+		{#each PARAGRAPHS as paragraph}
 			<div
-				class="flex flex-col md:flex-row {item.align === 'left'
+				class="flex flex-col md:flex-row {paragraph.image.align === 'left'
 					? 'md:flex-row-reverse'
 					: ''} items-center md:items-start"
 			>
 				<div class="md:w-1/4 p-4">
 					<img
-						src={item.image}
+						src={paragraph.image.src}
 						alt="Placeholder"
-						class={`w-full shadow-lg mask ${item.mask} ${item.mask === 'mask-square' ? 'rounded-3xl' : ''}`}
+						class={`w-full shadow-lg mask ${paragraph.image.mask} ${paragraph.image.mask === 'mask-square' ? 'rounded-3xl' : ''}`}
 					/>
 				</div>
 				<div class="md:w-1/4 p-4">
-					<p class="text-lg font-semibold">{item.text}</p>
+					<p class="text-lg font-semibold">{paragraph.text}</p>
 				</div>
 			</div>
 		{/each}
 	</div>
 </div>
-
 <Footer />
--- src/routes/courses/+page.svelte
+++ src/routes/courses/+page.svelte
@@ -1,12 +1,18 @@
 <script>
-	import Background from '../../components/utils/background.svelte';
 	import Navbar from '../../components/navigation/navbar.svelte';
 	import Footer from '../../components/navigation/footer.svelte';
+
+	const BACKGROUND = 'https://picsum.photos/seed/chaotic/1920/1080';
 </script>
 
 <Navbar />
-<Background background="https://picsum.photos/seed/chaotic/1920/1080" />
-
-<div class="flex-grow">some content</div>
+<div class="bg-[url('{BACKGROUND}')] bg-cover bg-center bg-fixed h-screen -z-10">
+	<div class="grid grid-cols-2 grid-rows-2 gap-20 place-items-center h-screen">
+		<div>1</div>
+		<div>2</div>
+		<div>3</div>
+		<div>4</div>
+	</div>
+</div>
 
 <Footer />
