4fee5a658a952f6d9cffe869f2dbbc46ebe8f63d
Adriano Saurwein <nano2101@outlook.com>
2024-10-06 18:13:30 +0200

    test upload to GHPages

--- svelte.config.js
+++ svelte.config.js
@@ -7,9 +7,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		prerender: {
+			handleHttpError: 'warn',
+		},
 		adapter: adapter(),
 		paths: {
-			base: '/ChaoticFamilyWebsite' | '/',
+			base: process.env.NODE_ENV === "development" ? '' : '/ChaoticFamilyWebsite',
 		}
 	}
 };
